83d5d2ef94a114f6fdbc21556f295b45ed26d488
João Justo <joao.justo@roit.com.br>
2021-06-25 19:00:05 -0300

    Updated database and started the relation between user and Appointment

--- src/database/migrations/1615860663060-CreateUsers.ts
+++ src/database/migrations/1615860663060-CreateUsers.ts
@@ -9,7 +9,7 @@ export default class CreateUsers1615860663060 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
